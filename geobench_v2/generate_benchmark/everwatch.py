b338d9f9659e54584f7ebf7284c9a225e6bfd4c7
Toolkit User <nils.lehmann24@gmail.com>
2025-03-24 14:33:52 +0000

    merge main

--- geobench_v2/generate_benchmark/everwatch.py
+++ geobench_v2/generate_benchmark/everwatch.py
@@ -3,16 +3,18 @@
 
 """Generate Benchmark version of EverWatch dataset."""
 
-from torchgeo.datasets import EverWatch
 import pandas as pd
 import os
 import argparse
+from geobench_v2.generate_benchmark.object_detection_util import (
+    process_dataset,
+    verify_patching,
+    compare_resize,
+    resize_object_detection_dataset,
+)
 
 
-
-def create_subset(
-    ds: EverWatch, metadata_df: pd.DataFrame, save_dir: str
-) -> None:
+def create_subset(metadata_df: pd.DataFrame, save_dir: str) -> None:
     """Create a subset of EverWatch dataset.
 
     Args:
@@ -25,19 +27,20 @@ def create_subset(
     # TODO
     # image comes in large tiles of 1500x1500 pixels, pass window size of potentially 750x750 and stride of 750 and resize to 512x512?
     # only train/test split so need to create a validation split but no geospatial metadata available, separate train and test csv file
+    pass
 
 
-def generate_metadata_df(ds: EverWatch) -> pd.DataFrame:
+def generate_metadata_df(root) -> pd.DataFrame:
     """Generate metadata DataFrame for EverWatch dataset."""
-    annot_df_train = pd.read_csv(
-        os.path.join(ds.root, ds.dir, 'train.csv')
-    )
+    annot_df_train = pd.read_csv(os.path.join(root, "train.csv"))
     annot_df_train["split"] = "train"
-    annot_df_test = pd.read_csv(
-        os.path.join(ds.root, ds.dir, 'test.csv')
-    )
+    annot_df_test = pd.read_csv(os.path.join(root, "test.csv"))
     annot_df_test["split"] = "test"
     annot_df = pd.concat([annot_df_train, annot_df_test], ignore_index=True)
+
+    annot_df = annot_df[
+        (annot_df["xmin"] != annot_df["xmax"]) & (annot_df["ymin"] != annot_df["ymax"])
+    ].reset_index(drop=True)
     return annot_df
 
 
@@ -54,13 +57,23 @@ def main():
     parser.add_argument(
         "--root", default="data", help="Root directory for EverWatch dataset"
     )
+    parser.add_argument(
+        "--save_dir",
+        default="geobenchV2/everwatch",
+        help="Directory to save the subset",
+    )
     args = parser.parse_args()
 
-    orig_dataset = EverWatch(root=args.root, download=False)
+    metadata_df = generate_metadata_df(args.root)
 
-    metadata_df = generate_metadata_df(orig_dataset)
+    path = os.path.join(args.root, "geobench_everwatch.parquet")
+    metadata_df.to_parquet(path)
 
-    metadata_df.to_parquet(f"{ds.root}/geobench_metadata.parquet")
+    # metadata_df = metadata_df.iloc[:10]
+
+    resize_object_detection_dataset(
+        args.root, metadata_df, args.save_dir, target_size=512
+    )
 
 
 if __name__ == "__main__":
