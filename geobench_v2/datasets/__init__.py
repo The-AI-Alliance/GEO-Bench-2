9bfa25c91b0a2eaa4b99fead2f8177ba1265d528
Nils Lehmann <35272119+nilsleh@users.noreply.github.com>
2025-04-04 08:57:09 +0200

    Add MMFlood Dataset (#34)
    * mmflood
    
    * files
    
    * more ifles

--- geobench_v2/datasets/__init__.py
+++ geobench_v2/datasets/__init__.py
@@ -20,6 +20,7 @@ from .biomassters import GeoBenchBioMassters
 from .mados import GeoBenchMADOS
 from .dynamic_earthnet import GeoBenchDynamicEarthNet
 from .sen4agrinet import GeoBenchSen4AgriNet
+from .mmflood import GeoBenchMMFlood
 
 __all__ = (
     "GeoBenchCaFFe",
@@ -39,4 +40,5 @@ __all__ = (
     "GeoBenchMADOS",
     "GeoBenchDynamicEarthNet",
     "GeoBenchSen4AgriNet",
+    "GeoBenchMMFlood",
 )
