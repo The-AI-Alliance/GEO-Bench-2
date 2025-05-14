Fix normalization scheme SatMAE (#56)
* updated scheme

* stash

* new stat computaiton

* stash

* updated normalization class

* clean

* notebooks for normalization in depth explanation

* cleanup

* updates

* updated caffe test

* fix caffe

* fix caffe

* clean up

* bunch of stuff to separate normalizing schemes

* changes

* run ruff

* run ruff and improve notebooks

* check required stats keys

* check required stats keys

geobench_v2/datamodules/caffe.py
@@ -9,6 +9,7 @@ from typing import Any
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import tacoreader
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -72,12 +73,13 @@ class GeoBenchCaFFeDataModule(GeoBenchSegmentationDataModule):
         Returns:
             pandas DataFrame with metadata.
         """
-        return pd.read_parquet(
-            os.path.join(self.kwargs["root"], "geobench_caffe.parquet")
+        self.data_df = tacoreader.load(
+            [os.path.join(self.kwargs["root"], f) for f in GeoBenchCaFFe.paths]
         )
+        return self.data_df
 
     def visualize_batch(
-        self, split: str = "train"
+        self, batch: dict[str, Tensor] | None = None, split: str = "train"
     ) -> tuple[plt.Figure, dict[str, Tensor]]:
         """Visualize a batch of data.
 
@@ -87,14 +89,16 @@ class GeoBenchCaFFeDataModule(GeoBenchSegmentationDataModule):
         Returns:
             The matplotlib figure and the batch of data
         """
-        if split == "train":
-            batch = next(iter(self.train_dataloader()))
-        elif split == "validation":
-            batch = next(iter(self.val_dataloader()))
-        else:
-            batch = next(iter(self.test_dataloader()))
+        if batch is None:
+            if split == "train":
+                batch = next(iter(self.train_dataloader()))
+            elif split == "validation":
+                batch = next(iter(self.val_dataloader()))
+            else:
+                batch = next(iter(self.test_dataloader()))
 
-        batch = self.data_normalizer.unnormalize(batch)
+        if hasattr(self.data_normalizer, "unnormalize"):
+            batch = self.data_normalizer.unnormalize(batch)
 
         images = batch["image"]
         masks = batch["mask"]

geobench_v2/datasets/dotav2.py
@@ -12,7 +12,8 @@ import torch.nn as nn
 from torch import Tensor
 from torchgeo.datasets import DOTA
 
-from .data_util import ClipZScoreNormalizer, DataUtilsMixin, DataNormalizer
+from .data_util import DataUtilsMixin
+from .normalization import ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -60,7 +61,7 @@ class GeoBenchDOTAV2(DOTA, DataUtilsMixin):
         root: Path,
         split: str,
         band_order: list[str] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         bbox_orientation: Literal["horizontal", "oriented"] = "oriented",
         transforms: nn.Module | None = None,
     ) -> None:
@@ -73,7 +74,7 @@ class GeoBenchDOTAV2(DOTA, DataUtilsMixin):
                 specify ['red', 'green', 'blue', 'blue'], the dataset would return images with 4 channels
                 in that order. This is useful for models that expect a certain band order, or
                 test the impact of band order on model performance.
-            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ClipZScoreNormalizer`,
+            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ZScoreNormalizer`,
                 which applies z-score normalization to each band.
             transforms:
         """

geobench_v2/datasets/everwatch.py
@@ -11,7 +11,8 @@ import torch.nn as nn
 from torch import Tensor
 from torchgeo.datasets import EverWatch
 
-from .data_util import ClipZScoreNormalizer, DataUtilsMixin, DataNormalizer
+from .data_util import DataUtilsMixin
+from .normalization import DataNormalizer, ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -49,19 +50,19 @@ class GeoBenchEverWatch(EverWatch, DataUtilsMixin):
         root: Path,
         split: str,
         band_order: list[str] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         transforms: nn.Module | None = None,
     ) -> None:
         """Initialize EverWatch dataset.
 
         Args:
             root: Path to the dataset root directory
-            split: The dataset split, supports 'train', 'val', 'test'
+            split: The dataset split, supports 'train', 'validation', 'test'
             band_order: The order of bands to return, defaults to ['red', 'green', 'blue'], if one would
                 specify ['red', 'green', 'blue', 'blue'], the dataset would return images with 4 channels
                 in that order. This is useful for models that expect a certain band order, or
                 test the impact of band order on model performance.
-            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ClipZScoreNormalizer`,
+            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ZScoreNormalizer`,
                 which applies z-score normalization to each band.
             transforms:
         """
@@ -74,11 +75,14 @@ class GeoBenchEverWatch(EverWatch, DataUtilsMixin):
 
         self.annot_df = pd.read_csv(os.path.join(self.root, "resized_annotations.csv"))
 
-        # remove all entries where xmin == xmax or ymin == ymax
         self.annot_df = self.annot_df[
             (self.annot_df["xmin"] != self.annot_df["xmax"])
             & (self.annot_df["ymin"] != self.annot_df["ymax"])
         ].reset_index(drop=True)
+
+        self.annot_df = self.annot_df[self.annot_df["split"] == self.split].reset_index(
+            drop=True
+        )
 
         # group per image path to get all annotations for one sample
         self.annot_df["sample_index"] = pd.factorize(self.annot_df["image_path"])[0]

geobench_v2/datasets/kuro_siwo.py
@@ -13,7 +13,7 @@ import torch.nn as nn
 from torch import Tensor
 
 from .base import GeoBenchBaseDataset
-from .data_util import ClipZScoreNormalizer
+from .normalization import ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -75,10 +75,10 @@ class GeoBenchKuroSiwo(GeoBenchBaseDataset):
         root: str,
         split: Literal["train", "val", "test"],
         band_order: dict[str, Sequence[str]] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         transforms: type[nn.Module] = None,
         return_stacked_image: bool = False,
-        time_step: Sequence[str] = ["pre_1","pre_2","post"],
+        time_step: Sequence[str] = ["pre_1", "pre_2", "post"],
         download: bool = False,
     ) -> None:
         """Initialize Kuro Siwo Dataset.
@@ -103,10 +103,12 @@ class GeoBenchKuroSiwo(GeoBenchBaseDataset):
         self.return_stacked_image = return_stacked_image
         if len(time_step) == 0:
             raise ValueError(
-                    "time_step must include at least one item from  ['pre_1, , 'pre_2', 'post']"
-                )
+                "time_step must include at least one item from  ['pre_1, , 'pre_2', 'post']"
+            )
         for i in time_step:
-            assert i in ['pre_1', 'pre_2', 'post'], "time_step must include at least one item from  ['pre_1, , 'pre_2', 'post']"
+            assert i in ["pre_1", "pre_2", "post"], (
+                "time_step must include at least one item from  ['pre_1, , 'pre_2', 'post']"
+            )
         self.time_step = time_step
 
     def __getitem__(self, index: int) -> dict[str, Tensor]:

geobench_v2/datasets/qfabric.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 from torch import Tensor
 
 from .base import GeoBenchBaseDataset
-from .data_util import ClipZScoreNormalizer
+from .normalization import ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -74,7 +74,7 @@ class GeoBenchQFabric(GeoBenchBaseDataset):
         root: Path,
         split: str,
         band_order: Sequence[str] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         transforms: nn.Module | None = None,
         time_steps: Sequence[int] = [0, 1, 2, 3, 4],
         metadata: Sequence[str] | None = None,
@@ -89,7 +89,7 @@ class GeoBenchQFabric(GeoBenchBaseDataset):
                 specify ['red', 'green', 'blue', 'nir', 'nir'], the dataset would return images with 5 channels
                 in that order. This is useful for models that expect a certain band order, or
                 test the impact of band order on model performance.
-            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ClipZScoreNormalizer`,
+            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ZScoreNormalizer`,
                 which applies z-score normalization to each band.
             transforms:
             time_steps: QFabric contains 5 time steps, this allows to select which time steps to use. Specified time steps

geobench_v2/datasets/resisc45.py
@@ -10,7 +10,8 @@ import torch.nn as nn
 from torch import Tensor
 from torchgeo.datasets import RESISC45
 
-from .data_util import ClipZScoreNormalizer, DataUtilsMixin
+from .data_util import DataUtilsMixin
+from .normalization import ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -37,7 +38,7 @@ class GeoBenchRESISC45(RESISC45, DataUtilsMixin):
         root: Path,
         split: str,
         band_order: Sequence["str"] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         **kwargs,
     ):
         """Initialize Resisc45 Dataset.
@@ -49,7 +50,7 @@ class GeoBenchRESISC45(RESISC45, DataUtilsMixin):
                 specify ['g', 'r', 'b', 'b], the dataset would return the green band first, then the red band,
                 and then the blue band twice. This is useful for models that expect a certain band order, or
                 test the impact of band order on model performance.
-            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ClipZScoreNormalizer`,
+            data_normalizer: The data normalizer to apply to the data, defaults to :class:`data_util.ZScoreNormalizer`,
                 which applies z-score normalization to each band.
             **kwargs: Additional keyword arguments passed to ``torchgeo.datasts.RESISC45``
         """

geobench_v2/datasets/spacenet2.py
@@ -15,7 +15,7 @@ from shapely import wkt
 from torch import Tensor
 
 from .base import GeoBenchBaseDataset
-from .data_util import ClipZScoreNormalizer
+from .normalization import ZScoreNormalizer
 from .sensor_util import DatasetBandRegistry
 
 
@@ -99,7 +99,7 @@ class GeoBenchSpaceNet2(GeoBenchBaseDataset):
         root: Path,
         split: str,
         band_order: list[str] = band_default_order,
-        data_normalizer: type[nn.Module] = ClipZScoreNormalizer,
+        data_normalizer: type[nn.Module] = ZScoreNormalizer,
         label_type: Literal["instance_seg", "semantic_seg"] = "semantic_seg",
         transforms: nn.Module = None,
         metadata: Sequence[str] | None = None,