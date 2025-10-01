b1713d8f7b3cd984ae6350f18981e089ea943b65
Paolo Fraccaro <paolo.fraccaro@ibm.com>
2025-10-01 10:16:03 +0200

    fix normaliser

--- geobench_v2/datasets/base.py
+++ geobench_v2/datasets/base.py
@@ -81,7 +81,7 @@ class GeoBenchBaseDataset(NonGeoDataset, DataUtilsMixin):
 
         if isinstance(data_normalizer, type):
             print(f"Initializing normalizer from class: {data_normalizer.__name__}")
-            if issubclass(data_normalizer, DataNormalizer):
+            if issubclass(data_normalizer, MultiModalNormalizer) | issubclass(data_normalizer, DataNormalizer):
                 self.data_normalizer = data_normalizer(
                     self.normalization_stats, self.band_order
                 )
