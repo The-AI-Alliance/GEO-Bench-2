3fce574d7898857692d57fd6f2e25ea56a2bd451
paolo fraccaro <paolo.fraccaro@ibm.com>
2025-10-27 10:59:41 +0000

    fix typo

--- tests/datamodules/test_so2sat.py
+++ tests/datamodules/test_so2sat.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 GeoBenchV2. All rights reserved.
 # Licensed under the Apache License 2.0.
 
-"""BenV2 Tests."""
+"""So2Sat Tests."""
 
 import os
 from collections.abc import Sequence
