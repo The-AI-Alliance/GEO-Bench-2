7bfc0aa5655dfd47fd8f2162ddcffc97d82b980a
piperwolters <piperw@allenai.org>
2026-04-29 18:22:36 +0000

    Also needed this rename of the HF name

--- geobench_v2/cli/download_geobenchV2.sh
+++ geobench_v2/cli/download_geobenchV2.sh
@@ -115,9 +115,9 @@ download_dataset() {
             hf download "aialliance/treesatai" "README.md" --repo-type=dataset --local-dir "$target_dir"
             ;;
         "so2sat")
-            hf download "aialliance/so2sat" "geobench_so2sat.tortilla" --repo-type=dataset --local-dir "$target_dir" 
-            hf download "aialliance/so2sat" "so2sat_stats_satmae.json" --repo-type=dataset --local-dir "$target_dir"
-            hf download "aialliance/so2sat" "so2sat_stats_clip_rescale.json" --repo-type=dataset --local-dir "$target_dir"
+            # HF repo aialliance/so2sat only publishes the tortilla + README; normalization
+            # stats live in geobench_v2.datasets.so2sat.GeoBenchSo2Sat.normalization_stats.
+            hf download "aialliance/so2sat" "geobench_so2sat.tortilla" --repo-type=dataset --local-dir "$target_dir"
             hf download "aialliance/so2sat" "README.md" --repo-type=dataset --local-dir "$target_dir"
             ;;
         "forestnet")
@@ -127,7 +127,7 @@ download_dataset() {
             hf download "aialliance/forestnet" "README.md" --repo-type=dataset --local-dir "$target_dir"
             ;;
         "nzcattle")
-            hf download "aialliance/nzcattle" "geobench_forestnet.tortilla" --repo-type=dataset --local-dir "$target_dir" 
+            hf download "aialliance/nzcattle" "geobench_nzcattle.tortilla" --repo-type=dataset --local-dir "$target_dir" 
             hf download "aialliance/nzcattle" "nzcattle_stats_satmae.json" --repo-type=dataset --local-dir "$target_dir"
             hf download "aialliance/nzcattle" "nzcattle_stats_clip_rescale.json" --repo-type=dataset --local-dir "$target_dir"
             hf download "aialliance/nzcattle" "README.md" --repo-type=dataset --local-dir "$target_dir"
